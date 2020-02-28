96bfef2ce14280a0c61796bdc7769fe73d6b549e
Tobias Okkenhaug Norum <tobnor12@live.no>
2020-02-28 14:18:17 +0100

    Initializes Unity
    mulig relevant info, itj alltid nødvendig

---
+++ Assets/Scripts/CON_Random_0_100.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CON_Random_0_100 : MonoBehaviour
+    {
+    int smallNumber = 20;
+    int maxMediumNumber = 80;
+    int bigNumber = 80;
+
+    void Start()
+    {
+
+
+        int randomNumber = Random.Range(0, 100);
+        Debug.Log(randomNumber);
+
+        if (randomNumber < smallNumber)
+        {
+            print("This Number Is Small");
+
+        }
+        else if (randomNumber < maxMediumNumber)
+        {
+            print("This Number Is Medium");
+
+        }
+        else
+        {
+            print("This Number Is Big");
+
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    
+    public int common;
+    public int rare;
+    public int legendary;
+    public Text commonText;
+    public Text rareText;
+    public Text legendaryText;
+}
